ae10981062db2cc02d90fd4267059c9859a5cbf8
Shizcow <pohl.devin@gmail.com>
2020-05-24 22:24:04 -0600

    gitignore

--- src/lib.rs
+++ src/lib.rs
@@ -4,14 +4,9 @@ use proc_macro::TokenStream;
 use quote::quote;
 use proc_macro2::{Ident, Span};
 
-fn override_function(mut input: syn::ItemFn) -> TokenStream {
-    // build.rs does all the rest
-    TokenStream::from(quote! {
-	#input
-    })
-}
 
-fn override_method(mut input: syn::ItemImpl) -> TokenStream {
+#[allow(unused)] // TODO
+fn override_method_for_flags(mut input: syn::ItemImpl) -> TokenStream {
     // step over the methods
     for mut item in &mut input.items {
 	match item {
@@ -39,25 +34,47 @@ fn override_method(mut input: syn::ItemImpl) -> TokenStream {
 #[proc_macro_attribute]
 pub fn override_default(attr: TokenStream, input: TokenStream) -> TokenStream {
     syn::parse_macro_input!(attr as syn::parse::Nothing); // I take no args
-
-    if let Ok(item) = syn::parse::<syn::ItemImpl>(input.clone()) {
-	override_method(item)
-    } else if let Ok(item) = syn::parse::<syn::ItemFn>(input) {
-	override_function(item)
-    } else {
-	panic!("I can't parse this yet");
-    }
+    input // build.rs will take care of this
 }
 
+fn default_impl(mut input: syn::ItemImpl) -> TokenStream {
+    // First, grab the struct name
+    let self_type = match input.self_ty.as_ref() {
+	syn::Type::Path(path) => path,
+	_ => panic!("Could not get Path for impl (should never see this)"),
+    }.path.segments[0].ident.to_string();
 
+    // then step over each method, appending override flag to each
+    for mut item in &mut input.items {
+	match item {
+	    syn::ImplItem::Method(method) => {
+		let override_flag = syn::Ident::new(
+		    &format!("__override_method_{}_{}",
+			     self_type,
+			     &method.sig.ident), Span::call_site());
+		method.attrs.push(
+		    syn::parse2::<syn::DeriveInput>(
+			quote! {
+			    #[cfg(not(#override_flag))]
+			    struct Dummy;
+			}
+		    ).unwrap().attrs.swap_remove(0));
+	    },
+	    _ => panic!("I can't overload anything other than methods in an impl block yet"),
+	}
+    }
+    TokenStream::from(quote! {
+	#input
+    })
+}
 
 fn default_function(mut input: syn::ItemFn) -> TokenStream {
-    let override_flag = syn::LitStr::new(&format!("func_{}", &input.sig.ident), Span::call_site());
+    let override_flag = proc_macro2::Ident::new(&format!("__override_func_{}", &input.sig.ident), Span::call_site());
     
     input.attrs.push(
 	syn::parse2::<syn::DeriveInput>(
 	    quote! {
-		#[cfg(not(overriden = #override_flag))]
+		#[cfg(not(#override_flag))]
 		struct Dummy;
 	    }
 	).unwrap().attrs.swap_remove(0));
@@ -71,7 +88,7 @@ pub fn default(attr: TokenStream, input: TokenStream) -> TokenStream {
     syn::parse_macro_input!(attr as syn::parse::Nothing); // I take no args
 
     if let Ok(item) = syn::parse::<syn::ItemImpl>(input.clone()) {
-	panic!("TODO"); //default_method(item)
+	default_impl(item)
     } else if let Ok(item) = syn::parse::<syn::ItemFn>(input) {
 	default_function(item)
     } else {
