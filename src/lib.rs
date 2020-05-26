5ccb94d01f174012f970fea5dbd8921361ccea3e
Shizcow <pohl.devin@gmail.com>
2020-05-25 20:04:22 -0600

    Cleaned trash from lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,35 +1,6 @@
-#![allow(unused)]
-
 use proc_macro::TokenStream;
 use quote::quote;
-use proc_macro2::{Ident, Span};
-
-
-#[allow(unused)] // TODO
-fn override_method_for_flags(mut input: syn::ItemImpl) -> TokenStream {
-    // step over the methods
-    for mut item in &mut input.items {
-	match item {
-	    syn::ImplItem::Method(method) => {
-		// edit the method names
-		method.sig.ident = 
-		    Ident::new(&format!("__{}_default", &method.sig.ident), Span::call_site());
-		// and add the #[inline] attribute
-		method.attrs.push(
-		    syn::parse2::<syn::DeriveInput>(
-			quote! {
-			    #[inline]
-			    struct Dummy;
-			}
-		    ).unwrap().attrs.swap_remove(0));
-	    },
-	    _ => panic!("I can't overload anything other than methods yet"),
-	}
-    }
-    TokenStream::from(quote! {
-	#input
-    })
-}
+use proc_macro2::Span;
 
 #[proc_macro_attribute]
 pub fn override_default(attr: TokenStream, input: TokenStream) -> TokenStream {
@@ -45,7 +16,7 @@ fn default_impl(mut input: syn::ItemImpl) -> TokenStream {
     }.path.segments[0].ident.to_string();
 
     // then step over each method, appending override flag to each
-    for mut item in &mut input.items {
+    for item in &mut input.items {
 	match item {
 	    syn::ImplItem::Method(method) => {
 		let override_flag = syn::Ident::new(
@@ -94,43 +65,4 @@ pub fn default(attr: TokenStream, input: TokenStream) -> TokenStream {
     } else {
 	panic!("I can't parse this yet");
     }
-}
-
-fn parse_args_strings(args: TokenStream) -> Vec<String> {
-    let mut strings = Vec::new();
-    for (i, arg) in args.into_iter().enumerate() {
-	if let proc_macro::TokenTree::Literal(lit) = arg { // is it a literal?
-	    let lit_str = lit.to_string();
-	    if i % 2 == 1 || // is it a _string_ literal?
-	    {
-		let mut chars = lit_str.chars();
-		chars.next().unwrap() != '"' || chars.last().unwrap() != '"'
-	    }{
-		panic!("Unexpected token '{}' (expected string literal)", lit.to_string());
-	    }
-	    strings.push(lit_str.chars() // great, add to list
-			    .skip(1).take(lit_str.len()-2)
-			    .collect::<String>());
-	    continue;
-	} else if let proc_macro::TokenTree::Punct(punct) = arg { // is it punctuation?
-	    if punct.to_string() != "," { // is it a comma? (for list)
-		panic!("Unexpected punctuation {} (expected comma)", punct.to_string());
-	    } else if i % 2 == 0 { // is it in the right place?
-		panic!("Expected list element");
-	    }
-	} else {
-	    panic!("Unexpected token: {}", arg.to_string());
-	}
-    }
-    strings
-}
-
-#[proc_macro]
-pub fn watch_files(args: TokenStream) -> TokenStream {
-    // first, parse the arguement list
-    let file_names = parse_args_strings(args);
-    // Now file_names is populated with Strings. Time to read the files
-    
-    panic!("{:?}", file_names);
-    TokenStream::new()
 }
